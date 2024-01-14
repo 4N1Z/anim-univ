066dcf8087abbbb869778bc43249477ea8eae387
4N1Z <20am014@sctce.ac.in>
2024-01-14 17:29:55 +0530

    initial : imports and card details

---
+++ app/utils/export.ts
@@ -0,0 +1,62 @@
+export const univ = [
+    {
+        title : "SKODA AUTO UNIVERSITY",
+        desc : "Skoda Auto University is a specialized higher education institution located in Mladá   Boleslav, Czech Republic. Established in 2000, it is closely affiliated with Skoda Auto, one of the leading automobile manufacturers in Europe.",
+        intake : "February - September",
+        courses :"Corporate finance in international business",
+        fee : "2550/yr",
+        image : "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+        color : "#FA5300"
+
+    },
+    {
+        title : "TOMAS BATA UNIVERSITY ",
+        desc : "At TBU in ZLIN , students receive a high quality education for affordable fees in all levels of study ( bachelor, master and doctoral ) . The university provides optimal conditions : highly qualified teachers and  excellent study facilities equipped with state-of-art technology",
+        intake : "September 2024 ",
+        courses :"•	Management  and marketing",
+        fee : "54050/yr",
+        image : "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+        color : "#9700FF"
+
+    },
+    {
+        title : "BRNO UNIVERSITY OF TECHNOLOGY ",
+        desc : "with its state-of-the-art research facilities  and academic excellence ,BUT has a distinguished reputation among students and professionals  . BUT is the nation&apos;s second largest  technical university",
+        intake : "February - September",
+        courses :"•	The fine art and design program ",
+        fee : "2550 euro",
+        image : "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
+        color : "#FA5300"
+
+    },
+    {
+        title : "CZECH TECHNICAL UNIVERSITY",
+        desc : "-  One of the largest and Oldest technical university in Europe. 454 in QS world university, Affordable living in dormitories,Career Counseling: Gain skills that will help you find your dream job. Cooperate with one of 50 mentors from different companies and professions.",
+        intake : "February - September",
+        courses :"Building structures engineering  ",
+        fee : "132,000 CZK",
+        image : "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
+        color : "#9700FF"
+
+    },
+    {
+        title : "CZU UNIVERSITY -  ",
+        desc : "Czech University of Life Sciences Prague is an agricultural education and research university in Prague, the Czech Republic Studies of agriculture were established at the Czech Technical University (ČVUT) in 1906, and the first agricultural engineers graduated in 1911. In 1920 the Faculty of Agriculture and Forestry was established, and in 1952 the faculty became an independent institution, the University College of Agriculture in Prague. In 1966 it moved to a newly built campus in Prague School, where it has been located since",
+        intake : "February - September",
+        courses :"	Business administration   ",
+        fee : "42000 CZK",
+        image : "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
+        color : "#FA5300"
+
+    },
+    {
+        title : "MENDEL UNIVERSITY",
+        desc : "With its state-of-the-art research facilities and academic excellence, BUT has a distinguished reputation among students and professionals. With its 17 thousand students and 8 faculties, BUT is the nation's second largest technical university",
+        intake : "February - September",
+        courses :"Economics and management   ",
+        fee : "650,000 CZK",
+        image : "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
+        color : "#9700FF"
+
+    },
+]
